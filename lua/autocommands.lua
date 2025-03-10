dac2be1b24a69d61cca66742a52e39910f80077c
Wade Bowers <wade.bowers@statenews.com>
2025-03-10 10:44:43 -0400

    move commentstring to an autocommand

--- lua/autocommands.lua
+++ lua/autocommands.lua
@@ -11,3 +11,10 @@ vim.api.nvim_create_autocmd('TextYankPost', {
     vim.highlight.on_yank()
   end,
 })
+
+vim.api.nvim_create_autocmd('FileType', {
+  pattern = 'twig',
+  callback = function()
+    vim.bo.commentstring = '{# %s #}'
+  end,
+})
